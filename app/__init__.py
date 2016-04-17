d394867dd3bccfd0fe1480dcaf89a739afd2a016
Stephen Wong <codetreklog@gmail.com>
2016-04-17 17:04:28 +0800

    Fix: remove unwanted Flask-Bootstrap dep

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.bootstrap import Bootstrap
 
 from config import config
 
 
 db = SQLAlchemy()
-bootstrap = Bootstrap()
 
 
 def create_app(config_name):
@@ -25,7 +23,6 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     db.init_app(app)
-    bootstrap.init_app(app)
 
     # Attach routes and custom error pages here
     from .main import main as main_blueprint
